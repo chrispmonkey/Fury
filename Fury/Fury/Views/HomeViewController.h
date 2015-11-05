7e1f8ebd741413fa470e518afe2b6e74b356f209
Chris Price <chrispmonkey@gmail.com>
2015-11-05 03:38:57 -0500

    Slowly getting closer to the pinterest look and feel for the feed that I want

--- Fury/Fury/Views/HomeViewController.h
+++ Fury/Fury/Views/HomeViewController.h
@@ -8,9 +8,13 @@
 
 #import <UIKit/UIKit.h>
 #import "FlatUIKit.h"
+#import "PSCollectionView.h"
+#import "SRRefreshView.h"
 
-@interface HomeViewController : UIViewController<UITextFieldDelegate, FUIAlertViewDelegate>
+@interface HomeViewController : UIViewController<UITextFieldDelegate, FUIAlertViewDelegate,UIScrollViewDelegate,PSCollectionViewDataSource,PSCollectionViewDelegate, SRRefreshDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+@property (strong,nonatomic) PSCollectionView *waterFLowView;
+@property (strong, nonatomic) SRRefreshView *slimeView;
 
 @end
